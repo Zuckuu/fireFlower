41591afd860e4ac6c754ff6a90a8ccbc3ebebd9e
Zuckuu <entbit12@gmail.com>
2022-09-08 12:38:04 -0500

    setting up the mod

---
+++ src/main/java/com/zucku/fireflower/item/ModItems.java
@@ -0,0 +1,15 @@
+package com.zucku.fireflower.item;
+
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.item.Item;
+import com.zucku.fireflower.FireFlower;
+
+
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS_DEFERRED_REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, FireFlower.MOD_ID);
+        
+}

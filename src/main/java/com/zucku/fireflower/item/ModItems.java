e40691ca8cc6bf328d0645106228413cc305e4c6
Zuckuu <entbit12@gmail.com>
2022-09-08 15:26:22 -0500

    adding fire flower item

--- src/main/java/com/zucku/fireflower/item/ModItems.java
+++ src/main/java/com/zucku/fireflower/item/ModItems.java
@@ -4,6 +4,8 @@ package com.zucku.fireflower.item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import com.zucku.fireflower.FireFlower;
 
@@ -13,6 +15,7 @@ public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FireFlower.MOD_ID);
     
+    public static final RegistryObject<Item> FIREFLOWER = ITEMS.register("fireflower", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
